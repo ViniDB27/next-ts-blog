05d61e2f01df3238a58c396c4fca243ec1a1dd59
Vinicnio Brejinski <viniciosdb@gmail.com>
2025-09-26 21:18:10 -0300

    post terminate

--- src/utils/format-datetime.ts
+++ src/utils/format-datetime.ts
@@ -1,20 +1,30 @@
 import {
   format,
   formatDistanceToNow as dateFnsFormatDistanceToNow,
-} from 'date-fns'
-import { ptBR } from 'date-fns/locale'
+} from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
-export function formatDatetime(rowDate: string) {
-  const date = new Date(rowDate)
-  return format(date, "dd/MM/yyyy 'ás' HH'h'mm", {
+export function formatDatetime(rawDate: string): string {
+  const date = new Date(rawDate);
+
+  return format(date, "dd/MM/yyyy 'às' HH'h'mm", {
     locale: ptBR,
-  })
+  });
 }
 
-export function formatDistanceToNow(rowDate: string) {
-  const date = new Date(rowDate)
+export function formatDistanceToNow(rawDate: string): string {
+  const date = new Date(rawDate);
+
   return dateFnsFormatDistanceToNow(date, {
     locale: ptBR,
     addSuffix: true,
-  })
+  });
+}
+
+export function formatHour(timestampMs: number): string {
+  const date = new Date(timestampMs);
+
+  return format(date, 'HH:mm:ss', {
+    locale: ptBR,
+  });
 }
